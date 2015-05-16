c3ac2e6f75cd496eb04ac5df79d581c23f29dfd9
Suui <suui.gd@gmail.com>
2015-05-17 00:37:42 +0200

    Initial C++ GameMode and BP added

---
+++ Source/UE4Tutorials/FancyGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "FancyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4TUTORIALS_API AFancyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
