c4066665923e293f56ebcca26d8a22cac7caca6b
Suui <suui.gd@gmail.com>
2015-05-17 01:39:49 +0200

    Defining NPCs

---
+++ Source/UE4Tutorials/NPC.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "NPC.generated.h"
+
+UCLASS()
+class UE4TUTORIALS_API ANPC : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Collision)
+	class USphereComponent* ProxSphere;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = NPCMessage)
+	FString NpcMessage;
+
+	// Sets default values for this character's properties
+	ANPC();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+};
