0a636d319560ca009f640b23099c9fcfa2dfd8c9
Suui <suui.gd@gmail.com>
2015-05-29 03:29:52 +0200

    Before deleting unnecessary classes

---
+++ Source/UE4Tutorials/LearningCPP/GUI/MyPlayerCameraManager.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Camera/PlayerCameraManager.h"
+#include "MyPlayerCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4TUTORIALS_API AMyPlayerCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/UE4Tutorials/LearningCPP/ItemPickup/MyWorldSettings.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/WorldSettings.h"
+#include "MyWorldSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4TUTORIALS_API AMyWorldSettings : public AWorldSettings
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
