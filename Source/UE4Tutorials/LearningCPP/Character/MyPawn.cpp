a0c0e1e4c06cb3d3de193595dfad7750127765dc
Suui <suui.gd@gmail.com>
2015-05-29 03:24:45 +0200

    Preparing Project for a Source reorganization in folders

---
+++ Source/UE4Tutorials/LearningCPP/Character/MyPawn.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4Tutorials.h"
+#include "MyPawn.h"
+
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
---
+++ Source/UE4Tutorials/LearningCPP/MyClass.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class UE4TUTORIALS_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
