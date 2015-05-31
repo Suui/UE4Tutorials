22b40ffcc37a980c4c4529f6d19a173fba1ad405
Suui <suui.gd@gmail.com>
2015-05-31 02:17:56 +0200

    Monster properties setup

---
+++ Source/UE4Tutorials/LearningCPP/Mobs/LCPPMonster.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "LCPPMonster.generated.h"
+
+UCLASS()
+class UE4TUTORIALS_API ALCPPMonster : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+	float Speed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+	int32 Health;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+	int32 ExperienceDrop;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+	UClass* Loot;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+	int32 BaseAttackDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+	float BaseAttackCD;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = MonsterProperties)
+	float TimeSinceLastBaseAttack;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = MonsterProperties)
+	USphereComponent* SightSphereComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = MonsterProperties)
+	USphereComponent* BasicAttackRangeSphereComp;
+
+	// Sets default values for this character's properties
+	ALCPPMonster();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	
+	
+};
