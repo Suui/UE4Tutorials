16208699e857a21e9c486feaee4a8fb94d1f98a0
Suui <suui.gd@gmail.com>
2015-06-11 01:30:22 +0200

    Spell header and class implemented

--- Source/UE4Tutorials/LearningCPP/Spells/LCPPSpell.cpp
+++ Source/UE4Tutorials/LearningCPP/Spells/LCPPSpell.cpp
@@ -2,6 +2,7 @@
 
 #include "UE4Tutorials.h"
 #include "LCPPSpell.h"
+#include "LearningCPP/Mobs/LCPPMonster.h"
 
 
 // Sets default values
@@ -10,7 +11,17 @@ ALCPPSpell::ALCPPSpell()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ParticleVisualization = CreateDefaultSubobject<UParticleSystemComponent>("Particle Visualization");
+	RootComponent = ParticleVisualization;
+
+	ProxBox = CreateDefaultSubobject<UBoxComponent>("Proximity Box");
+	ProxBox->AttachTo(RootComponent);
+
+	Duration = 3.f;
+	TimeAlive = 0.f;
+	DamagePerSec = 1;
 }
+
 
 // Called when the game starts or when spawned
 void ALCPPSpell::BeginPlay()
@@ -19,10 +30,32 @@ void ALCPPSpell::BeginPlay()
 	
 }
 
-// Called every frame
-void ALCPPSpell::Tick( float DeltaTime )
-{
-	Super::Tick( DeltaTime );
 
+// Called every frame
+void ALCPPSpell::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	TArray<AActor*> ActorsAffected;
+	ProxBox->GetOverlappingActors(ActorsAffected);
+
+	for (int i = 0; i < ActorsAffected.Num(); ++i)
+	{
+		if (ActorsAffected[i] != Caster)
+		{
+			ALCPPMonster* Monster = Cast<ALCPPMonster>(ActorsAffected[i]);
+			if (Monster != nullptr && ProxBox->IsOverlappingComponent(Monster->CapsuleComponent))
+				Monster->TakeDamage(DamagePerSec * DeltaTime, FDamageEvent(), nullptr, this);
+		}
+	}
+
+	TimeAlive += DeltaTime;
+	if (TimeAlive > Duration) Destroy();
 }
 
+
+void ALCPPSpell::SetCaster(AActor* Caster)
+{
+	this->Caster = Caster;
+	AttachRootComponentTo(Caster->GetRootComponent());
+}
--- Source/UE4Tutorials/LearningCPP/Spells/LCPPSpell.h
+++ Source/UE4Tutorials/LearningCPP/Spells/LCPPSpell.h
@@ -5,12 +5,29 @@
 #include "GameFramework/Actor.h"
 #include "LCPPSpell.generated.h"
 
+
 UCLASS()
 class UE4TUTORIALS_API ALCPPSpell : public AActor
 {
 	GENERATED_BODY()
 	
-public:	
+public:
+
+	AActor* Caster;
+	float TimeAlive;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = SpellProperties)
+	UBoxComponent* ProxBox;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = SpellProperties)
+	UParticleSystemComponent* ParticleVisualization;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = SpellProperties)
+	int32 DamagePerSec;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = SpellProperties)
+	float Duration;
+
 	// Sets default values for this actor's properties
 	ALCPPSpell();
 
@@ -18,8 +35,7 @@ public:
 	virtual void BeginPlay() override;
 	
 	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
+	virtual void Tick(float DeltaSeconds) override;
 
-	
-	
+	void SetCaster(AActor* Caster);
 };
