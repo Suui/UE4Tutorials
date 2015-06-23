03a781846e19cc4d8be8d904430092ba1baa44eb
Suui <suui.gd@gmail.com>
2015-06-23 22:50:16 +0200

    Initial Stage

---
+++ Source/UE4Tutorials/UE4ProgrammingGuide/GameControlledCameras/PGCameraDirector.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "PGCameraDirector.generated.h"
+
+UCLASS()
+class UE4TUTORIALS_API APGCameraDirector : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APGCameraDirector();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
