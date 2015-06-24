bf19f6b109e2f9084348e7887f623cc872b2682e
Suui <suui.gd@gmail.com>
2015-06-24 04:43:15 +0200

    Implemented: Countdown and Explosion (+BP)

--- Source/UE4Tutorials/UE4ProgrammingGuide/VariablesTimersAndEvents/PGCountdown.h
+++ Source/UE4Tutorials/UE4ProgrammingGuide/VariablesTimersAndEvents/PGCountdown.h
@@ -10,8 +10,34 @@ class UE4TUTORIALS_API APGCountdown : public AActor
 {
 	GENERATED_BODY()
 
+	UTextRenderComponent* CountdownText;
+	FTimerHandle CountdownTimerHandle;
+
+
+public:
+
+	// How long the countdown will run (in seconds)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 CountdownTime;
+
+
+private:
+
+	void UpdateTimerDisplay();
+
+	void AdvanceTimer();
+
+
+protected:
+
+	UFUNCTION(BlueprintNativeEvent)
+	void CountdownHasFinished();
+	virtual void CountdownHasFinished_Implementation();
+
 
 public:
 
 	APGCountdown();
+
+	void BeginPlay() override;
 };
