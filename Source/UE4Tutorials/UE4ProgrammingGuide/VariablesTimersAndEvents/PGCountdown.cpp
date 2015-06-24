2c6feaf85b8ec1cb6c9015e7badc36664303e96f
Suui <suui.gd@gmail.com>
2015-06-24 02:43:56 +0200

    Initial Stage

---
+++ Source/UE4Tutorials/UE4ProgrammingGuide/VariablesTimersAndEvents/PGCountdown.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4Tutorials.h"
+#include "PGCountdown.h"
+
+
+APGCountdown::APGCountdown()
+{
+
+}
---
+++ Source/UE4Tutorials/UE4ProgrammingGuide/VariablesTimersAndEvents/PGCountdown.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "PGCountdown.generated.h"
+
+UCLASS()
+class UE4TUTORIALS_API APGCountdown : public AActor
+{
+	GENERATED_BODY()
+
+
+public:
+
+	APGCountdown();
+};
